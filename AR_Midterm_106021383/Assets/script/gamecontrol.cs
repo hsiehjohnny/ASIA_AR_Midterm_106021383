33357cd3bb3f943be922cbdfc8dec110f0f316f4
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2020-11-17 05:17:54 +0800

    finish

---
+++ AR_Midterm_106021383/Assets/script/gamecontrol.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class gamecontrol : zom
+{
+    public Transform dogknight;
+    public Transform zombie;
+
+    public FixedJoystick Joystick;
+
+    public Animator anidog;
+    public Animator anizombie;
+
+
+    public bool dogruning = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dogruning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        dogknight.Translate(0, 0, Joystick.Vertical * 0.01f);
+
+        if (Joystick.Vertical != 0)
+        {
+            dogruning = true;
+            if (dogruning == true)
+            {
+                anidog.SetBool("dogruning", true);
+            }
+        }
+        else
+        {
+            dogruning = false;
+            if (dogruning == false)
+            {
+               anidog.SetBool("dogruning", false);
+            }
+        }
+    }
+    public void anicontroll(string anicall)
+    {
+        if (anicall == "dogatk")
+        {
+            if (zom.canatk == false)
+            {
+                anidog.SetTrigger(anicall);
+                anizombie.SetTrigger("zomhit");
+            }
+            if (zom.canatk == true)
+            {
+                anizombie.SetTrigger("zomatk");
+                anidog.SetTrigger("doghit");
+            }
+        }
+        else
+        {
+            anidog.SetTrigger("dogrun");
+            anizombie.SetTrigger("zomrun");
+        }
+    }
+}
---
+++ AR_Midterm_106021383/Assets/script/zom.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class zom : MonoBehaviour
+{
+    static public bool canatk = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        if (other.name == "dog")
+        {
+            print(canatk);
+            canatk = false;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        canatk = true;
+
+    }
+}
